72af1fb66be77081cbb068bf1b0c49be16c1211e
harshit1507 <harshit1507@yahoo.com>
2021-01-29 19:57:34 +0530

    Laser Defender Completed

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    Text healthText;
+    Player1 player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText = GetComponent<Text>();
+        player = FindObjectOfType<Player1>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = player.GetHealth().ToString();
+    }
+}
---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    // config params
+    [Header("Player")]
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] int health = 200;
+
+    [Header("SFX")]
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] [Range(0, 1)] float deathSoundVolume = 0.75f;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.25f;
+
+
+    [Header("Projectile")]
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
+
+    float xMin, xMax, yMin, yMax;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+        //StartCoroutine(PrintAndWAit());
+    }    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+       
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinously());
+        }
+        if(Input.GetButtonUp("Fire1"))
+        {
+            //StopAllCoroutines();    // It will stop all coroutines 
+
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinously()
+    {
+        // Instantiate() => Used to create a gameObject in run time at desired location with desired rotation;
+       while(true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position, shootSoundVolume);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+        
+    }
+
+    private void Move()
+    {
+        // Time.deltaTime => It gives the time duration of a single frame
+
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        // Initialize Boundaries for the player
+        // ViewPortToWorldPoint() => Convert the position of something as it relates to the camera view into the world's space view.
+
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Coroutine => A method which can suspend its execution until the suspend (yield) instructions are met.
+    // e.g. => when player gets to zero health, start the KillPlayer coroutine (method instructions)
+
+    //IEnumerator PrintAndWAit()
+    //{
+    //    Debug.Log("First message sent");
+    //    yield return new WaitForSeconds(3);
+    //    Debug.Log("The second messafe");
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if(!damageDealer)
+        {
+            return;
+        }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<Level>().LoadGameOver();
+        Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+}
